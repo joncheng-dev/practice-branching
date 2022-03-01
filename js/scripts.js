89c978e76b182003aedf9c4075c35c7a263e62cc
Jonathan Cheng <jcheng461@gmail.com>
2022-03-01 16:57:24 +0900

    fix script so that function branch is called upon page load, and then again upon clicking of button

--- js/scripts.js
+++ js/scripts.js
@@ -25,29 +25,7 @@ $(document).ready(function () {
     }
   }
 
-  $("#click").on("click", function () {
-    const animal = parseInt(
-      prompt(
-        "Which animal did you want to read about? Enter 1 for Cheetahs, 2 for Elephants, or 3 for Foxes"
-      )
-    );
+  $("#click").on("click", branch);
 
-    if (animal === 1) {
-      $("#cheetahs").show();
-      $("#elephants").hide();
-      $("#foxes").hide();
-    } else if (animal === 2) {
-      $("#cheetahs").hide();
-      $("#elephants").show();
-      $("#foxes").hide();
-    } else if (animal === 3) {
-      $("#cheetahs").hide();
-      $("#elephants").hide();
-      $("#foxes").show();
-    } else {
-      $("#cheetahs").show();
-      $("#elephants").hide();
-      $("#foxes").hide();
-    }
-  });
+  branch();
 });
