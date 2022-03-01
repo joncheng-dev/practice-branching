03ef75679a4c0e40292db31025719f648e3176e5
Jonathan Cheng <jcheng461@gmail.com>
2022-03-01 13:05:17 +0900

    WIP: practice branching in jquery by creating div for 3 animals. Each animal has its own div. Each is hidden. User can click to move hide/show information

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  function branch() {
+    const animal = parseInt(
+      prompt(
+        "Which animal did you want to read about? Enter 1 for Cheetahs, 2 for Elephants, or 3 for Foxes"
+      )
+    );
+
+    if (animal === 1) {
+      $("#cheetahs").show();
+      $("#elephants").hide();
+      $("#foxes").hide();
+    } else if (animal === 2) {
+      $("#cheetahs").hide();
+      $("#elephants").show();
+      $("#foxes").hide();
+    } else if (animal === 3) {
+      $("#cheetahs").hide();
+      $("#elephants").hide();
+      $("#foxes").show();
+    } else {
+      $("#cheetahs").show();
+      $("#elephants").hide();
+      $("#foxes").hide();
+    }
+  }
+
+  $("#click").on("click", function () {
+    const animal = parseInt(
+      prompt(
+        "Which animal did you want to read about? Enter 1 for Cheetahs, 2 for Elephants, or 3 for Foxes"
+      )
+    );
+
+    if (animal === 1) {
+      $("#cheetahs").show();
+      $("#elephants").hide();
+      $("#foxes").hide();
+    } else if (animal === 2) {
+      $("#cheetahs").hide();
+      $("#elephants").show();
+      $("#foxes").hide();
+    } else if (animal === 3) {
+      $("#cheetahs").hide();
+      $("#elephants").hide();
+      $("#foxes").show();
+    } else {
+      $("#cheetahs").show();
+      $("#elephants").hide();
+      $("#foxes").hide();
+    }
+  });
+});
